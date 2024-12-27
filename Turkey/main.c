a7954afe557686c40ceae0d7ec36406fe752d6eb
Edwin Cardenas <eacardenase@gmail.com>
2024-12-27 13:00:32 -0500

    feat: add turkey recipe

--- Turkey/main.c
+++ Turkey/main.c
@@ -8,7 +8,25 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    // Declare the variable called 'weight' of type float
+    float weight;
+    
+    // Store a number in that variable
+    weight = 14.2;
+    
+    // Log it to the user
+    printf("The turkey weighs %f.\n", weight);
+    
+    // Declare another variable of type float
+    float cookingTime;
+    
+    // Calculate the cooking time and store it in the variable
+    // In this case, '*' means 'multiplied by'
+    cookingTime = (15.0 + 15.0) * weight;
+    
+    // Log that to the user
+    printf("Cook it for %f minutes.\n", cookingTime);
+    
+    // End this function and indicate success
     return 0;
 }
